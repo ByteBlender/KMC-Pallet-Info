9666842b85b1d1d8937b7603361b018a9afa5b43
GAYAN-PROBOOK\Gayan <thebyteblender@gmail.com>
2020-10-27 22:29:51 +0800

    Add project files.

---
+++ KMC Pallet Info/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace KMC_Pallet_Info
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtPalletID = new System.Windows.Forms.TextBox();
+            this.btnViewDetails = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtPalletID
+            // 
+            this.txtPalletID.Location = new System.Drawing.Point(230, 75);
+            this.txtPalletID.Name = "txtPalletID";
+            this.txtPalletID.Size = new System.Drawing.Size(112, 20);
+            this.txtPalletID.TabIndex = 0;
+            // 
+            // btnViewDetails
+            // 
+            this.btnViewDetails.Location = new System.Drawing.Point(365, 77);
+            this.btnViewDetails.Name = "btnViewDetails";
+            this.btnViewDetails.Size = new System.Drawing.Size(75, 23);
+            this.btnViewDetails.TabIndex = 1;
+            this.btnViewDetails.Text = "View Details";
+            this.btnViewDetails.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(177, 82);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(47, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Pallet ID";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnViewDetails);
+            this.Controls.Add(this.txtPalletID);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtPalletID;
+        private System.Windows.Forms.Button btnViewDetails;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
